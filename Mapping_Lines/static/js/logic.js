f5519616cb96c8cd5383c39ef07fc33b7b2e5d2c
Emerset Farquharson <emersetfarquharson@Emersets-Laptop.local>
2022-12-01 22:54:20 -0500

    Updated Mapping Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,39 +1,29 @@
 // 14.2.4
 // Add console.log to check to see if our code is working.
-console.log("Multiple Points working");
+console.log("Mapping Lines working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+let map = L.map('mapid').setView([37.6214, -122.3790], 5);
 
-// 14.4.1
-//  Add a marker to the map for Los Angeles, California.
-//L.circleMarker([34.0522, -118.2437], {
-//  color: 'black',
-//  fillColor: 'yellow',
-//  radius: 300
-//}).addTo(map);
+// 14.4.3
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6214, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
 
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-  .addTo(map);
- });
+// 14.4.3
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
         {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
