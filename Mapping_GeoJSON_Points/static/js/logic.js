4dfb18e6cb2651e7134bec782940ec8cd0601abf
Emerset Farquharson <emersetfarquharson@Emersets-Laptop.local>
2022-12-01 23:49:18 -0500

    Added GeoJSON layer

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,29 +1,46 @@
 // 14.2.4
 // Add console.log to check to see if our code is working.
-console.log("Mapping Lines working");
+console.log("Mapping GeoJSON Points working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6214, -122.3790], 5);
+// 14.5.2
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// 14.4.3
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6214, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
+// 14.5.2
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"14",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// 14.4.3
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
+// 14.5.2
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport,
+  {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup();
+     }
+  }
+  ).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
         {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
